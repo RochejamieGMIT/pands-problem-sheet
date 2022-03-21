5ba9d646d5f01ff0ffe22f3e3f0ab612b70e87b0
unknown <G00411347@gmit.ie>
2022-03-21 21:32:12 +0000

    Fixing up some code- weeks 3 and 5

--- weeklyTask03-secondString.py
+++ weeklyTask03-secondString.py
@@ -2,27 +2,6 @@
 # Write a program that asks a user to input a string and outputs every second letter in reverse order.
 
 # Author: Jamie Roche 
-inputString = input('enter a string:')
-
-lenghtOfString = len(inputString) - 1
-count = 0 # using count to ensurereprint starts at last character 
-even = 0 # printing every second charater using even/odd
-out = ''
-
-# using the remained of the number of letters left in the stirng divided by 2 to determine if the 
-# number is even or odd, allows us to print every second letter. Adding only even numbers to the output
-# string 
-
-while lenghtOfString >= 0:
-    if even == 0:
-     out = out + (inputString[lenghtOfString])
-     
-    lenghtOfString = lenghtOfString - 1
-    count += 1
-    even = count % 2
-
- 
-print(out)
 
 ############################################################################
 # Redo of code to minimise the amout of lines required ot complete the task
--- weeklyTask05-weekday.py
+++ weeklyTask05-weekday.py
@@ -8,32 +8,12 @@
 
 from datetime import datetime
 
-Day = datetime.today().weekday() # read day as variable 
-#Day = 0-6 to test
-
-days = ("Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",
-"Sunday"
-)
-
-today = days[Day] # selecting day to print out as string based on number of the day of the week.
-
-if Day >= 5:
-    print ("Today is ", today, "it is the weekend, yay!")
-else :
-    print ("Today is ", today, "Yes, unfortunately It is a Weekday")
-
-
 ######################################################################
 # Redid the code to minimise lines 
 # I liked original using the tuple for the week days, but it's less efficient 
 
 today2 = datetime.today().strftime('%A') # Take the day of the week as a string
-#print(today2) - Test if day is rad in correctly 
+#print(today2) - Test if day is read in correctly 
 
 if today2 == "Saturday" or today2 == "Sunday": #Test if the string matches the weekend days
     print ("Today is ", today2, "it is the weekend, yay!")
